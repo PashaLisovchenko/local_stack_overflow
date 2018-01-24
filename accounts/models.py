4b0a080e87dc788ee9e4f0c7394657f32cd604ca
pasha <lisovchenko.pasha@gmail.com>
2018-01-24 17:25:38 +0200

    ADD: base models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    link_github = models.URLField(max_length=100, blank=True)
+    image = models.ImageField(blank=True, null=True, upload_to='profile_image/')
+    bio = models.TextField(max_length=500, blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
+
+    # def get_absolute_url(self):
+    #     return reverse('account:profile', args=[self.user.username])
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
---
+++ questionnaire/models.py
@@ -0,0 +1,57 @@
+import datetime
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.db import models
+from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
+from django.core.urlresolvers import reverse
+from django.contrib.contenttypes.models import ContentType
+
+
+class Question(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=60)
+    slug = models.SlugField(max_length=60, db_index=True)
+    text_question = models.TextField(max_length=500)
+    tags = TaggableManager()
+    users_like = models.ManyToManyField(User, related_name='question_liked', blank=True)
+    # dislike
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.user.username + ', ' + self.slug
+
+    # def get_absolute_url(self):
+    #     return reverse('question:detail_question', args=[self.id, self.slug])
+
+
+class Answer(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE) # неуверен что нужен on_delete
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    text_answer = models.TextField(max_length=500)
+    is_correct = models.BooleanField(default=False)
+    users_like = models.ManyToManyField(User, related_name='answer_liked', blank=True)
+    # dislike
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username + str(self.id)
+
+    # def get_absolute_url(self):
+    #     return reverse('question:detail_question', args=[self.id, self.slug])
+
+
+# class Comment(models.Model):
+#     content_type = models.ForeignKey(ContentType)
+#     object_id = models.PositiveIntegerField()
+#     content_object = GenericForeignKey('content_type', 'object_id')
+#     user = models.ForeignKey(User, related_name='comments')
+#     comment = models.CharField(max_length=300)
+#     added_at = models.DateTimeField(default=datetime.datetime.now)
+#
+#     class Meta:
+#         ordering = ('-added_at',)
