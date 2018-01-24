b1e056b9420501e52cfdbabadbfe7881c8a7e4c3
pasha <lisovchenko.pasha@gmail.com>
2018-01-24 17:26:05 +0200

    ADD: register models in admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from .models import Profile
 
-# Register your models here.
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['show_git_url', 'bio', 'birth_date', 'location', ]
+
+    def show_git_url(self, obj):
+        return format_html("<a href='{url}'>{url}</a>", url=obj.link_github)
+
+    show_git_url.short_description = "GitHub URL"
+
+
+admin.site.register(Profile, ProfileAdmin)
+
