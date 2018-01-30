2d5d59085c6e179a7a524169b0c2760fca168c36
pasha <lisovchenko.pasha@gmail.com>
2018-01-30 17:25:26 +0200

    Add: get_best_tags [find popular tag]

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models import Count
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.urlresolvers import reverse
+from taggit.models import Tag
+
+from questionnaire.models import Question
 
 
 class Profile(models.Model):
@@ -14,11 +18,20 @@ class Profile(models.Model):
     birth_date = models.DateField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        ordering=('-created', )
+
     def __str__(self):
         return self.user.username
 
     def get_absolute_url(self):
-        return reverse('account:profile', args=[self.user.id])
+        return reverse('accounts:profile', args=[self.user.id])
+
+    def get_best_tags(self):
+        questions = Question.objects.filter(user=self.user)
+        tags = Tag.objects.filter(question__in=questions)
+        best_tags = tags.annotate(same_tags=Count('name')).order_by('-same_tags')[:3]
+        return best_tags
 
 
 @receiver(post_save, sender=User)
