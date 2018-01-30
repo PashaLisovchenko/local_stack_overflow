632bda404625fc42b6c24cf4dcf9847d0725e18a
pasha <lisovchenko.pasha@gmail.com>
2018-01-30 17:29:05 +0200

    ADD: new urls for views

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -1,9 +1,19 @@
 from django.conf.urls import url
-from .views import QuestionList, TagList, UserList, QuestionListByTag
+from .views import QuestionList, TagList, UserList, QuestionListByTag, QuestionDetail, current_answer,CommentAddAnswer,\
+    CommentAddQuestion, AddQuestionView, suggest_users
 
 urlpatterns = [
     url(r'^$', QuestionList.as_view(), name="home_page"),
     url(r'^tags/$', TagList.as_view(), name="tags"),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$', QuestionListByTag.as_view(), name="tag_detail"),
+    url(r'^(?P<id>\d+)/(?P<slug>[-\w]+)/$', QuestionDetail.as_view(), name='question_detail'),
     url(r'^users/$', UserList.as_view(), name="users"),
+
+    url(r'^add-question/$', AddQuestionView.as_view(), name="add_question"),
+
+    url(r'^add-comment-answer/(?P<id>\d+)/$', CommentAddAnswer.as_view(), name="comment-add-answer"),
+    url(r'^add-comment-question/(?P<id>\d+)/$', CommentAddQuestion.as_view(), name="comment-add-question"),
+
+    url(r'^ajax/correct-answer/$', current_answer, name='correct_answer'),
+    url(r'^ajax/user/$', suggest_users, name='find_user'),
 ]
