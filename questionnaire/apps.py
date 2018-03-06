bb24883956c42dfdb410c7e6a13fbe9f765f6f78
LisovchenkoPasha <lisovchenko.pasha@gmail.com>
2018-03-06 12:54:09 +0200

    ADD: register models to actstream

--- questionnaire/apps.py
+++ questionnaire/apps.py
@@ -3,3 +3,11 @@ from django.apps import AppConfig
 
 class QuestionnaireConfig(AppConfig):
     name = 'questionnaire'
+
+    def ready(self):
+        from actstream import registry
+        registry.register(self.get_model('Question'))
+        registry.register(self.get_model('Answer'))
+        registry.register(self.get_model('Comment'))
+        from django.contrib.auth.models import User
+        registry.register(User)
